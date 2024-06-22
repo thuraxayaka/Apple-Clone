386dbb6be19408504c7ad28e6c97734d070097c0
Thura Htet Zaw <thurahtetzaw02@gmail.com>
2024-06-22 20:05:25 +0630

    Finish

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+import Highlight from './components/Highlight';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
+const App = () => {
+  
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Highlight/>
+      <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import {heroVideo,smallHeroVideo} from '../utils';
+const Hero = () => {
+  const [videoSrc,setVideoSrc] = React.useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo);
+  const handleVideoSrcSet = () => {
+    if(window.innerWidth  < 760) {
+      setVideoSrc(smallHeroVideo)
+    }else {
+      setVideoSrc(heroVideo)
+    }
+  }
+  useGSAP(() => {
+    gsap.to('#hero', {
+      opacity: 1,
+      delay: 2
+    })
+
+    gsap.to('#cta',{
+      opacity: 1,
+      delay: 2,
+      y: -50
+    })
+  },[])
+
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+
+    return () => window.removeEventListener('resize',handleVideoSrcSet);
+  },[])
+  return (
+     <section className="w-full nav-height bg-black relative">
+        <div className="h-5/6 w-full flex-center flex-col">
+          <p className="hero-title" id="hero">Iphone 15 Pro</p>
+          <div className="md:w-10/12 w-9/12">
+            <video autoPlay muted  key={videoSrc} className="pointer-events-none">
+              <source src={videoSrc} type="video/mp4"/>
+            </video>
+          </div>
+        </div>
+        
+        <div id="cta"
+        className="flex flex-col items-center opacity-0 translate-y-20"
+        >
+          <a href="#highlights" className="btn">Buy</a>
+          <p className="font-normal text-xl">From $199/month or $999</p>
+        </div>
+     </section>
+  )
+}
+
+export default Hero
